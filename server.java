0b89afc0616432a23bf6d3af065ccdba0997bc2f
SW33TSTUFF <simaakniyaz@gmail.com>
2025-07-03 14:43:27 +0530

    Added server socket and client socket

--- server.java
+++ server.java
@@ -1,12 +1,54 @@
+import java.net.*;
+import java.io.*;
+
 public class Server {
+
+    private static ServerSocket serverSocket;
+    private static Socket clientSocket;
+    private static PrintWriter out;
+    private static BufferedReader in;
+
     public static void main(String[] args) {
         System.out.println("Middleware assignment");
         if(args.length < 1) {
             System.out.println("Please use the proper format (java Server port_number)");
-            
+            System.exit(1);
         }
 
         int portNumber = Integer.parseInt(args[0]);
         System.out.println("Entered port number: " + portNumber);
+
+        try {
+            serverSocket = new ServerSocket(portNumber);
+            if(serverSocket != null) {
+                System.out.println("Server is running in port " + portNumber);
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+            System.exit(1);
+        }
+
+        try {
+            clientSocket = serverSocket.accept();
+            if(clientSocket != null) {
+                System.out.println("Client connected");
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+            System.exit(1);
+        }
+
+
+        // now we need to read the data from client to server
+        // by using input stream we get the raw byte data from the client side
+        // then we need to convert that byte stream to text, so we have the char
+        // buffer reader then gives us the option to read line by line using readLine method
+
     }
 }
+
+
+
+
+// REFERENCES
+// https://www.baeldung.com/a-guide-to-java-sockets - for java sockets
