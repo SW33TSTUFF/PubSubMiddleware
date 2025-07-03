3eec38c04fa2ade691fc0d1f846649d4393fbca1
SW33TSTUFF <simaakniyaz@gmail.com>
2025-07-03 13:45:46 +0530

    Port number argument check

--- server.java
+++ server.java
@@ -1,5 +1,12 @@
 public class Server {
     public static void main(String[] args) {
         System.out.println("Middleware assignment");
+        if(args.length < 1) {
+            System.out.println("Please use the proper format (java Server port_number)");
+            
+        }
+
+        int portNumber = Integer.parseInt(args[0]);
+        System.out.println("Entered port number: " + portNumber);
     }
 }
